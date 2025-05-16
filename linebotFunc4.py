4d8fb181ca198058743d25ff788cc1097f45d49c
zongon987 <a112070037@mail.shu.edu.tw>
2025-05-16 14:20:16 +0800

    Add files via upload

---
+++ linebotFunc4.py
@@ -0,0 +1,130 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import request, abort
+from linebot import  LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, PostbackEvent, TextSendMessage, ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction, ImagemapArea, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction
+from urllib.parse import parse_qsl
+import datetime
+
+line_bot_api = LineBotApi('0eLufUSZ4U82p9VgIQHLjILrHh6srxGDc3DmQ9w0LU3VqHHPQERIba9rkM54APTUKTIDCK9O7NgM6fMpo9p8JAnWVg73BmB14nLLQfW4Jfk3fiFApGSebxke0Dllh6gq2tcdHWkdaqZJlxHWoDQe1gdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('2cdd63ef8b0b2776b23a21df0c147e85')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    mtext = event.message.text
+    if mtext == '@圖片地圖':
+        sendImgmap(event)
+
+    elif mtext == '@日期時間':
+        sendDatetime(event)
+
+@handler.add(PostbackEvent)  #PostbackTemplateAction觸發此事件
+def handle_postback(event):
+    backdata = dict(parse_qsl(event.postback.data))  #取得data資料
+    if backdata.get('action') == 'sell':
+        sendData_sell(event, backdata)
+
+def sendImgmap(event):  #圖片地圖
+    try:
+        image_url = 'https://i.imgur.com/Yz2yzve.jpg'  #圖片位址
+        imgwidth = 1040  #原始圖片寛度一定要1040
+        imgheight = 300
+        message = ImagemapSendMessage(
+            base_url=image_url,
+            alt_text="圖片地圖範例",
+            base_size=BaseSize(height=imgheight, width=imgwidth),  #圖片寬及高
+            actions=[
+                MessageImagemapAction(  #顯示文字訊息
+                    text='你點選了紅色區塊！',
+                    area=ImagemapArea(  #設定圖片範圍:左方1/4區域
+                        x=0, 
+                        y=0, 
+                        width=imgwidth*0.25, 
+                        height=imgheight  
+                    )
+                ),
+                URIImagemapAction(  #開啟網頁
+                    link_uri='http://www.e-happy.com.tw',
+                    area=ImagemapArea(  #右方1/4區域(藍色1)
+                        x=imgwidth*0.75, 
+                        y=0, 
+                        width=imgwidth*0.25, 
+                        height=imgheight  
+                    )
+                ),
+            ]
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendDatetime(event):  #日期時間
+    try:
+        message = TemplateSendMessage(
+            alt_text='日期時間範例',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
+                title='日期時間示範',
+                text='請選擇：',
+                actions=[
+                    DatetimePickerTemplateAction(
+                        label="選取日期",
+                        data="action=sell&mode=date",  #觸發postback事件
+                        mode="date",  #選取日期
+                        initial="2025-05-16",  #顯示初始日期
+                        min="2025-05-16",  #最小日期
+                        max="2026-05-16"  #最大日期
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取時間",
+                        data="action=sell&mode=time",
+                        mode="time",  #選取時間
+                        initial="10:00",
+                        min="00:00",
+                        max="23:59"
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取日期時間",
+                        data="action=sell&mode=datetime",
+                        mode="datetime",  #選取日期時間
+                        initial="2025-05-16T10:00",
+                        min="2025-05-16T00:00",
+                        max="2026-05-16T23:59"
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendData_sell(event, backdata):  #Postback,顯示日期時間
+    try:
+        if backdata.get('mode') == 'date':
+            dt = '日期為：' + event.postback.params.get('date')  #讀取日期
+        elif backdata.get('mode') == 'time':
+            dt = '時間為：' + event.postback.params.get('time')  #讀取時間
+        elif backdata.get('mode') == 'datetime':
+            dt = datetime.datetime.strptime(event.postback.params.get('datetime'), '%Y-%m-%dT%H:%M')  #讀取日期時間
+            dt = dt.strftime('{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  #轉為字串
+        message = TextSendMessage(
+            text=dt
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+if __name__ == '__main__':
+    app.run()
